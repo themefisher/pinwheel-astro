6e1f6412dcef3abddde2bdf0411fb17f41506960
Mamadou Diagne <genova@dofbi.com>
2023-11-23 02:38:05 +0000

    ♻️ refactor (components): homapageFeature

--- src/layouts/function-components/HomapageFeature.jsx
+++ src/layouts/function-components/HomapageFeature.jsx
@@ -13,11 +13,20 @@ const HomapageFeature = ({ feature_list }) => {
           >
             <div>
               <h3 className="h4 text-xl lg:text-2xl">{item.title}</h3>
-              <p>{item.content}</p>
+              <p>{item.activities} activités</p>
             </div>
             <span className="icon mt-4">
               <FeatherIcon />
             </span>
+            <div className="relative bg-gray-100 h-6 rounded-md mt-2">
+                <div
+                  className={`absolute h-full top-0 left-0 bg-${item.color || 'bg-blue-500'}`} // Utilisation de la couleur personnalisée ou bleu par défaut
+                  style={{ width: `${item.percentage}%` }}
+                ></div>
+                <div className="absolute bottom-0 right-0 h-full flex items-center pr-2">
+                  <span className="text-sm text-primary">{item.percentage}%</span>
+                </div>
+              </div>
           </div>
         );
       })}
