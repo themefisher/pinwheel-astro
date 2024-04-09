b2589d31587d0b5abdca9b2585a3f5dcb6c70e74
Mamadou Diagne <genova@dofbi.com>
2024-04-09 07:52:29 +0000

    ✨ feat (contentful): Add

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly CONTENTFUL_SPACE_ID: string;
+  readonly CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+}
