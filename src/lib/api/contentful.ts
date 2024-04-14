20827931fb7fbd5d52919e4550692ac775713724
Mamadou Diagne <genova@dofbi.com>
2024-04-14 01:39:40 +0000

    ♻️ refactor (api): env public

--- src/lib/api/contentful.ts
+++ src/lib/api/contentful.ts
@@ -16,9 +16,9 @@ export interface PublicationPost {
 }
 
 export const contentfulClient = contentful.createClient({
-  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  space: import.meta.env.PUBLIC_CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    ? import.meta.env.PUBLIC_CONTENTFUL_PREVIEW_TOKEN
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
