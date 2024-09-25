c351dfff0dbdee91614f7d23ea5750ddd006616d
Sanjana <sanjanasogimatt@gmail.com>
2024-09-25 21:31:53 +0530

    Updating brand colors and button

--- src/layouts/shortcodes/Button.jsx
+++ src/layouts/shortcodes/Button.jsx
@@ -7,8 +7,8 @@ const Button = ({
   rel = "noopener noreferrer" 
 }) => {
   const buttonStyles = {
-    primary: "bg-white text-black border border-black hover:bg-black hover:text-white",
-    secondary: "bg-black text-white border border-black hover:bg-white hover:text-black",
+    primary: "bg-palette-blue text-white border border-palette-navy hover:text-palette-lightBlue",
+    secondary: "bg-white text-palette-blue border border-palette-blue hover:text-palette-navy hover:border-palette-navy",
   };
 
   return (
@@ -16,6 +16,7 @@ const Button = ({
       href={href}
       target="_blank"
       rel={rel}
+      aria-label={`${children} (opens in a new tab)`}
       className={`inline-block px-5 py-3 rounded transition-all ${
         buttonStyles[style]
       }`}
