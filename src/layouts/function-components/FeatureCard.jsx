96c3c5c1f8cc8d1c909e2589e0b73a1111f49656
Sanjana <sanjanasogimatt@gmail.com>
2024-09-27 19:28:11 +0530

    Updating key in featurecard

--- src/layouts/function-components/FeatureCard.jsx
+++ src/layouts/function-components/FeatureCard.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 const FeatureCard = ({ feature_list, cardHeight, cardShadow , background }) => {
   return (
     <div className="key-feature-grid mt-10 grid grid-cols-2 gap-7 md:grid-cols-3 xl:grid-cols-4">
-      {feature_list.map((item, i) => {
+      {feature_list.map((item) => {
         return (
           <div
-            key={i}
+            key={item.id}
           >
             <div className={`feature-card rounded-xl ${cardHeight} ${cardShadow} ${background} p-5 pb-8 text-center`}>
               <h3 className="h4 text-xl lg:text-2xl">{item.title}</h3>
