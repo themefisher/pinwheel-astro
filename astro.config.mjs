84442811078e484baec3a5d333b1e76d7cfe1bdc
Rubel Hossain <rubel.themefisher@gmail.com>
2026-06-27 12:43:22 +0600

    chore: updated to Astro v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import { unified } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -73,6 +74,7 @@ export default defineConfig({
     mdx(),
   ],
   markdown: {
+    processor: unified(),
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: { theme: "one-dark-pro", wrap: true },
   },
